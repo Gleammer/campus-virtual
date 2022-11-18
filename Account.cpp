6b86d16c912288cf43a54763daa5a798ea49ec96
Gleammer <radulin4a@gmail.com>
2022-11-18 18:49:13 +0200

    Intern8

--- Account.cpp
+++ Account.cpp
@@ -5,7 +5,7 @@ Account::Account():name(""), username(""), email(""), age(0){}
 
 Account::Account(std::string name, std::string username, std::string email, int age):name(name), username(username), email(email), age(age){}
 
-std::string getName(){
+std::string Account::getName(){
     return(name);
 }
 
